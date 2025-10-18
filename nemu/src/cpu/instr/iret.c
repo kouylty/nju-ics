21b90ce9cda4851146a6295f38c5c4af1af1c50d
kouylty <kouyulin124@126.com>
2025-10-18 16:18:34 +0800

    4.1 Complete

--- nemu/src/cpu/instr/iret.c
+++ nemu/src/cpu/instr/iret.c
@@ -8,7 +8,7 @@ make_instr_func(iret)
     cpu.eip = vaddr_read(cpu.esp,SREG_SS,4);
     cpu.esp += 4;
     cpu.cs.val = vaddr_read(cpu.esp,SREG_SS,2);
-    cpu.esp += 2;
+    cpu.esp += 4;
     cpu.eflags.val = vaddr_read(cpu.esp,SREG_SS,4);
     cpu.esp += 4;
 #endif
